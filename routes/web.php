ce19a33c2b83ed26b33e6bad13ef02a189763564
aziyan99 <rajaazian08@gmail.com>
2023-09-04 11:45:57 +0700

    #1 add vue route

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// FIXME: Need to remove
+// Route::get('/testapi', function () {
+//     $response = \Illuminate\Support\Facades\Http::withHeaders([
+//             'API-Key' => config('taptalk.api_key'),
+//             'Content-Type' => 'application/json'
+//         ])->post(config('taptalk.send_message_api'), [
+//             'phone' => '+6282211223344',
+//             'messageType' => config('taptalk.message_type'),
+//             'body' => '123321'
+//     ]);
+
+//     if ($response->ok()) {
+//         $response->collect('status')->first();
+//         $response->collect('data')['id'];
+//     }
+// });
+
+Route::get('/auth/{vuejs?}', function () {
+    return view('layouts.auth');
+})->where('vuejs', '[\/\w\.-]*');

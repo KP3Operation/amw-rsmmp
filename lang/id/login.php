79cdac2be6d0db008a892fbda8e19d3f6768e984
aziyan99 <rajaazian08@gmail.com>
2023-09-07 22:30:01 +0700

    #1 add wrong otp message

--- lang/id/login.php
+++ lang/id/login.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'errros' => [
-        'wrong_phone_number' => 'No. Handphone yang diberikan salah.',
-        'wrong_otp_code' => 'Kode OTP yang diberikan salah.'
-    ]
-
+    "welcome_message" => "Selamat Datang Di Aviat Mobile Web",
+    "errros" => [
+        "wrong_phone_number" => "No. Handphone yang diberikan salah.",
+        "wrong_otp_code" => "Kode OTP yang diberikan salah."
+    ],
+    "otp_message" => "Kode OTP anda untuk masuk ke aviat web mobile adalah :otpcode",
 ];

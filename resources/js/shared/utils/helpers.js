e1399ac0a99093dfcb8a39b18c18666a18cd64ea
aziyan99 <rajaazian08@gmail.com>
2023-09-10 10:28:22 +0700

    #1 add helper

---
+++ resources/js/shared/utils/helpers.js
@@ -0,0 +1,6 @@
+export function getSecondsLeft (startDate, endDate) {
+    const startTimestamp = startDate.getTime();
+    const endTimestamp = endDate.getTime();
+    const millisecondsDiff = endTimestamp - startTimestamp;
+    return Math.floor(millisecondsDiff / 1000);
+}

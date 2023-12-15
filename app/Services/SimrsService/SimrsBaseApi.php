0a6f654698562b0b81ebc6c1d94214fe4396ca28
aziyan99 <rajaazian08@gmail.com>
2023-12-15 14:16:53 +0700

    use try-catch and only dry-run laravel pint

--- app/Services/SimrsService/SimrsBaseApi.php
+++ app/Services/SimrsService/SimrsBaseApi.php
@@ -2,34 +2,49 @@
 
 namespace App\Services\SimrsService;
 
+use App\Exceptions\SimrsException;
 use GuzzleHttp\Promise\PromiseInterface;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 
 class SimrsBaseApi implements ISimrsBaseApi
 {
+    /**
+     * @throws SimrsException
+     */
     public function get(string $url, array $options = [], array $query = []): PromiseInterface|Response
     {
-        $accessKey = config('simrs.access_key');
+        try {
+            $accessKey = config('simrs.access_key');
 
-        return Http::timeout(120)->withHeaders([
-            'Content-Type' => '',
-        ])->withOptions([
-            'verify' => false,
-        ] + $options)->get(config('simrs.base_url').$url, [
-            'AccessKey' => $accessKey,
-        ] + $query);
+            return Http::timeout(120)->withHeaders([
+                'Content-Type' => '',
+            ])->withOptions([
+                    'verify' => false,
+                ] + $options)->get(config('simrs.base_url').$url, [
+                    'AccessKey' => $accessKey,
+                ] + $query);
+        }catch (\Exception $e) {
+            throw new SimrsException('Gagal terhubung dengan SIMRS, mohon menghubungi tim support kami', 500);
+        }
     }
 
+    /**
+     * @throws SimrsException
+     */
     public function post(string $url, array $options = [], array $body = []): PromiseInterface|Response
     {
-        $accessKey = config('simrs.access_key');
+        try {
+            $accessKey = config('simrs.access_key');
+            return Http::timeout(120)->withHeaders([
+                'Content-Type' => '',
+            ])->withOptions([
+                    'verify' => false,
+                ] + $options)->post(config('simrs.base_url').$url, [
+                    'AccessKey' => $accessKey] + $body);
 
-        return Http::timeout(120)->withHeaders([
-            'Content-Type' => '',
-        ])->withOptions([
-            'verify' => false,
-        ] + $options)->post(config('simrs.base_url').$url, [
-            'AccessKey' => $accessKey] + $body);
+        }catch (\Exception $e) {
+            throw new SimrsException('Gagal terhubung dengan SIMRS, mohon menghubungi tim support kami', 500);
+        }
     }
 }
